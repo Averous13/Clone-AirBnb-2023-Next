24403d341b550c688cdc9f35120b5af205312737
aDibsyam <syambudiadib@gmail.com>
2026-09-13 10:53:49 +0700

    add: register-ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import RegisterModal from "./components/modal/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 const font = Inter(
   {subsets: ['latin']}
@@ -19,6 +21,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`h-full antialiased`}
     >
       <body className={`${font.className} min-h-full flex flex-col`}>
+        <ToasterProvider />
+        <RegisterModal/>
         <Navbar />
         {children}
       </body>
