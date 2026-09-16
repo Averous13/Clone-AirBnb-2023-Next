1f88290b65dd0797fbe1d147ae0f662cfeeeb034
aDibsyam <syambudiadib@gmail.com>
2026-09-16 21:13:51 +0700

    add: image-upload

--- app/components/modal/RentModal.tsx
+++ app/components/modal/RentModal.tsx
@@ -12,6 +12,7 @@ import { categories } from "../navbar/Categories";
 import Map from "../Map";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS {
   CATEGORY = 0,
@@ -55,6 +56,7 @@ const RentModal = () => {
   const guestCount = watch('guestCount');
   const roomCount = watch('roomCount');
   const bathroomCount = watch('bathroomCount');
+  const imageSrc = watch('imageSrc');
 
   const Map = useMemo(() => dynamic(() => import('../Map'), {
     ssr: false
@@ -137,6 +139,15 @@ const RentModal = () => {
           subtitle="How many bathrooms do you have?"
           value={bathroomCount}
           onChange={(value) => setCustomValue('bathroomCount', value)}/>
+      </div>
+    )
+  }
+
+  if (step === STEPS.IMAGES){
+    bodyContent = (
+      <div className="dlex flex-col gap-8">
+        <Heading title="Add a photo to your place" subtitle="Show guests what your place looks like!"/>
+        <ImageUpload value={imageSrc} onChange={(value) => setCustomValue('imageSrc', value)}/>
       </div>
     )
   }
